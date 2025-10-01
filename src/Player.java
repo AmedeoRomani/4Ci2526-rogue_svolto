e2a709e51d888f97061bd1500148e0657e907cb1
Amedeo <amedeo.romani@marconirovereto.it>
2025-10-01 12:00:15 +0200

    Aggiunta nuove funzionalità per la gestione degli oggetti e delle statistiche del giocatore

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Player {
-    
+
     private int hp;
     private final int HP_MAX;
 
@@ -49,4 +49,88 @@ public class Player {
         return finalFireRate;
     }
 
+    public void viewStats() {
+        System.out.printf("HP: %d Danno corrente %.2f2,firerate rate: %.2f \n", hp, finalDamage, finalDamage,
+                finalFireRate);
+    }
+
+    public void addItem(Item nuovo) {
+        if (nuovo instanceof Active) {
+            this.active = (Active) nuovo;
+        } else if (nuovo instanceof Passive passive) {
+            passiveList.add(passive);
+
+        } else {
+            this.trinket = (Trinket) nuovo;
+        }
+    }
+
+    public Trinket dropTrinket() {
+        if (this.trinket == null)
+            return null;
+        Trinket temp = this.trinket;
+        this.trinket = null;
+        return temp;
+    }
+
+    public boolean checkCollectibles() {
+        boolean trovati = false;
+        for (int i = collectList.size() - 1; i >= 0; i--) {
+            Collectible c = collectList.get(i);
+            if (c instanceof Trinket) {
+                collectList.remove(i);
+                trovati = true;
+            }
+
+        }
+        return trovati;
+    }
+
+    public boolean ricarica() {
+        boolean trovati = false;
+        for (int i = collectList.size() - 1; i >= 0; i--) {
+            Collectible c = collectList.get(i);
+            if (c instanceof Trinket) {
+                collectList.remove(i);
+                trovati = true;
+            }
+        }
+        return trovati;
+    }
+
+    public boolean cura() {
+        if (hp <= 0)
+            return false;
+        boolean curato = false;
+        for (int i = collectList.size() - 1; i >= 0; i--) {
+            Collectible c = collectList.get(i);
+            if (c instanceof Heart) {
+                curato = true;
+                if (hp < HP_MAX) {
+                    hp += 10;
+
+                }
+                if (hp > HP_MAX)
+                    hp = HP_MAX;
+                collectList.remove(i);
+               
+            }
+            
+        }
+        return curato;
+    }
+
+    public boolean recharge(Battery b) {
+        boolean BatteryFound = false;
+        for (int i = collectList.size() - 1; i > 0; i++) {
+            Collectible c = collectList.get(i);
+            if (c instanceof Battery) {
+                collectList.remove(c);
+                BatteryFound = true;
+                this.active.reload();
+                return true;
+            }
+        }
+        return BatteryFound;
+    }
 }
